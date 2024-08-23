b105f664a15e43b7215c5d93398cde4b8fc554f8
Eric Aagaard Davis <ericdavis@Erics-MacBook-Pro.local>
2024-08-23 20:14:57 +0200

    Setup folder structure

---
+++ PokeFinder/Views/Feature/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  PokeFinder
+//
+//  Created by Eric Aagaard Davis on 23/08/2024.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ PokeFinder/Views/Feature/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  PokeFinder
+//
+//  Created by Eric Aagaard Davis on 23/08/2024.
+//
+
+import Foundation
